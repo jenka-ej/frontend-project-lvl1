81ced6b156a909a655a20678588d93517497dc29
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-26 11:02:43 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
-import { username1 } from '../src/cli.js';
 import readlineSync from 'readline-sync';
-export const username3 = console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+import { username1 } from '../src/cli.js';
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 let result = 0;
 for (let i = 0; i < 3; i += 1) {
-	let rand = Math.floor(Math.random() * 98);
-	const username4 = console.log('Question: ' + rand);
-        const username5 = readlineSync.question('Your answer: ');
+  const rand = Math.floor(Math.random() * 98);
+  console.log('Question: ' + rand);
+  const username5 = readlineSync.question('Your answer: ');
 
-	if ((rand % 2 === 0 && username5 === "yes") || (rand % 2 !== 0 && username5 === "no")) {
-		console.log('Correct');
-		result += 1;
-        } else {
-		if (rand % 2 === 0) {
-			console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-			break;
-		} else if (rand % 2 !== 0) {
-			console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                        break;
-		}
-	}
-};
+  if ((rand % 2 === 0 && username5 === "yes") || (rand % 2 !== 0 && username5 === "no")) {
+    console.log('Correct');
+    result += 1;
+  } else {
+    if (rand % 2 === 0) {
+      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+      break;
+    } else if (rand % 2 !== 0) {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      break;
+    }
+  }
+}
+
 if (result === 3) {
-	console.log('Congratulations, ' + username1 + '!');
+  console.log('Congratulations, ' + username1 + '!');
 } else {
-        console.log("Let's try again, " + username1 + '!');
-};
+  console.log("Let's try again, " + username1 + '!');
+}
