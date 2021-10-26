5d12cc09cff5d66c992e6f3dd43aa3f85cd213ca
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-26 10:57:37 +0300

    Update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 
 import { username1 } from '../src/cli.js';
 
-const username3 = console.log('What is the result of the expression?');
+console.log('What is the result of the expression?');
 let result = 0;
 for (let i = 0; i < 1; i += 1) {
   const rand = Math.floor(Math.random() * 98);
@@ -15,7 +15,7 @@ for (let i = 0; i < 1; i += 1) {
   const expression1 = rand + rand1;
   const expression2 = rand2 - rand3;
   const expression3 = rand4 * rand5;
-  const username4 = console.log('Question: ' + rand + ' + ' + rand1);
+  console.log('Question: ' + rand + ' + ' + rand1);
   const username5 = readlineSync.question('Your answer: ');
 
   if (Number(username5) === expression1) {
@@ -26,7 +26,7 @@ for (let i = 0; i < 1; i += 1) {
     break;
   }
 
-  const username6 = console.log('Question: ' + rand2 + ' - ' + rand3);
+  console.log('Question: ' + rand2 + ' - ' + rand3);
   const username7 = readlineSync.question('Your answer: ');
 
   if (Number(username7) === expression2) {
@@ -37,9 +37,9 @@ for (let i = 0; i < 1; i += 1) {
     break;
   }
 
-  const username8 = console.log('Question: ' + rand4 + ' * ' + rand5);
+  console.log('Question: ' + rand4 + ' * ' + rand5);
   const username9 = readlineSync.question('Your answer: ');
-	
+
   if (Number(username9) === expression3) {
     console.log('Correct');
     result += 1;
@@ -47,10 +47,10 @@ for (let i = 0; i < 1; i += 1) {
     console.log("'" + username9 + "'" + " is wrong answer ;(. Correct answer was '" + expression3 + "'.");
     break;
   }
-};
+}
 
 if (result === 3) {
   console.log('Congratulations, ' + username1 + '!');
 } else {
   console.log("Let's try again, " + username1 + '!');
-};
+}
