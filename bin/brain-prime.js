2729c71ddedb005522a04d92fa0f3adba72e7802
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-26 15:36:56 +0300

    Update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -8,35 +8,33 @@ let result = 0;
 for (let i = 0; i < 3; i += 1) {
   let presult = 0;
   const check = () => {
-    let rand = Math.floor(Math.random() * 1000);
-    for (let i = 1; i <= rand; i += 1) {
-      if (rand % i === 0) {
+    const rand = Math.floor(Math.random() * 1000);
+    for (let m = 1; m <= rand; m += 1) {
+      if (rand % m === 0) {
         presult += 1;
       }
     }
     return rand;
   }
-	
-  let rand2 = check();
-  const username4 = console.log('Question: ' + rand2);
+
+  const rand2 = check();
+  console.log(`Question: ${rand2}`);
   const username5 = readlineSync.question('Your answer: ');
 
-  if ((username5 === "yes" && presult === 2) || (username5 === "no" && presult !== 2)) {
+  if ((username5 === 'yes' && presult === 2) || (username5 === 'no' && presult !== 2)) {
     console.log('Correct!');
     result += 1;
-  } else {
-    if (presult === 2) {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-      break;
-    } else if (presult !== 2) {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      break;
-    }
+  } else if (presult === 2) {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    break;
+  } else if (presult !== 2) {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    break;
   }
 }
 
 if (result === 3) {
-  console.log('Congratulations, ' + username1 + '!');
+  console.log(`Congratulations, ${username1}!`);
 } else {
-  console.log("Let's try again, " + username1 + '!');
+  console.log(`Let's try again, ${username1}!`);
 }
