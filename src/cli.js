cd97eaa89bba84a66ec58f6598434a202feaa99f
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-26 16:07:36 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 export const username = console.log('Welcome to the Brain Games!');
 export const username1 = readlineSync.question('May I have your name? ');
-export const username2 = console.log('Hello, ' + username1 + '!');
+export const username2 = console.log(`Hello, ${username1}!`);
