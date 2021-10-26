ad661cb1bd8cd53db07f16e505a4de30e979d797
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-26 15:24:51 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,25 +7,23 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".');
 let result = 0;
 for (let i = 0; i < 3; i += 1) {
   const rand = Math.floor(Math.random() * 98);
-  console.log('Question: ' + rand);
+  console.log(`Question: ${rand}`);
   const username5 = readlineSync.question('Your answer: ');
 
   if ((rand % 2 === 0 && username5 === 'yes') || (rand % 2 !== 0 && username5 === 'no')) {
-    console.log('Correct');
+    console.log('Correct!');
     result += 1;
-  } else {
-    if (rand % 2 === 0) {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-      break;
-    } else if (rand % 2 !== 0) {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      break;
-    }
+  } else if (rand % 2 === 0) {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    break;
+  } else if (rand % 2 !== 0) {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    break;
   }
 }
 
 if (result === 3) {
-  console.log('Congratulations, ' + username1 + '!');
+  console.log(`Congratulations, ${username1}!`);
 } else {
-  console.log("Let's try again, " + username1 + '!');
+  console.log(`Let's try again, ${username1}!`);
 }
