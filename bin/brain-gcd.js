1b9e64062e5c07cf89901daceab045c954a0f927
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-27 12:40:10 +0300

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -5,14 +5,14 @@ import { username1 } from '../src/cli.js';
 
 console.log('Find the greatest common divisor of given numbers.');
 let result = 0;
-let del = 0;
-let del1 = 0;
 for (let i = 0; i < 3; i += 1) {
   const rand = Math.floor(Math.random() * 100);
   const rand1 = Math.floor(Math.random() * 100);
   console.log(`Question: ${rand} ${rand1}`);
   const username5 = readlineSync.question('Your answer: ');
   const nod = (num, num1) => {
+    let del = 0;
+    let del1 = 0;
     if (num < num1) {
       for (let m = num; m !== 0; m -= 1) {
         if (num1 % m === 0 && num % m === 0) {
