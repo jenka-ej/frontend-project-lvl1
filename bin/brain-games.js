6968466f5b46b7fb5d2e195601533070e82008e3
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-27 17:48:25 +0300

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const username = console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!');
 const username1 = readlineSync.question('May I have your name? ');
-const username2 = console.log(`Hello, ${username1}!`);
+console.log(`Hello, ${username1}!`);
