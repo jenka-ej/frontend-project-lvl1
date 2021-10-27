a3c2bdf3333c7a0e8a67b7a4024e97ef78c29c23
jenka-ej <85373955+jenka-ej@users.noreply.github.com>
2021-10-27 13:17:25 +0300

    Update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -23,7 +23,7 @@ const censore = (array) => {
   answer = array[rand5];
   for (let i = 0; i < array.length; i += 1) {
     if (i === rand5) {
-      array[i] = '..';
+      array[i] = "..";
     }
   }
   return array;
